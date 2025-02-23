e04b3216396bf5e66923db9279b3d39e78aa35a9
Gi Ho <giquigeraldine@outlook.com>
2025-02-23 23:31:06 +0000

    add category to models

--- ecopoints/models.py
+++ ecopoints/models.py
@@ -3,12 +3,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(max_length=255)
     score = models.PositiveSmallIntegerField()
-    category = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
