bb59242ca252290aa601af49eff71e9b254c4496
Kimberley Bates <2358219b@student.gla.ac.uk>
2025-02-23 20:43:25 +0000

    set up Task and CompletedTask models, developed Insights view logic

--- ecopoints/models.py
+++ ecopoints/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(max_length=255)
+    score = models.PositiveSmallIntegerField()
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class CompletedTask(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} completed {self.task.name}"
