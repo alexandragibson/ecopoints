2a0af97be17678e78793b33f247ed12e7e30f539
Kimberley Bates <2358219b@student.gla.ac.uk>
2025-03-13 10:11:33 +0000

    Histogram and bubble chart for Insights page

--- ecopoints/urls.py
+++ ecopoints/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from ecopoints import views
+from .views import insights, get_bubble_data
 
 app_name = 'ecopoints'
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('insights/', views.insights, name='insights'),
+    path('bubble-data/<int:month>/', get_bubble_data, name='bubble-data'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('category/<slug:category_slug>/', views.show_category, name='show_category'),
     path('task/complete/<int:task_id>/', views.complete_task, name='complete_task'),
