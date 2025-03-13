0af08d4b353e4e4718aa26623986c2761dd82ddc
Gi Ho <giquigeraldine@outlook.com>
2025-03-13 11:48:37 +0000

    fix jquery synax

--- static/js/ecopoints-query.js
+++ static/js/ecopoints-query.js
@@ -1,21 +1,23 @@
-//$(document).ready(function()) {
+$(document).ready(function () {
+  hover_colour = '#62a162';
 
-    hover_colour = '#62a162';
-
-    $('a').hover(
-        function() {
-            $(this).css('color', hover_colour);
-        },
-        function() {
-            $(this).css('color', 'white');
+  $('a').hover(
+    function () {
+      $(this).css('color', hover_colour);
+    },
+    function () {
+      $(this).css('color', 'white');
     });
 
-    $('li').hover(
-        function() {
-            $(this).css('color', hover_colour);
-        },
-        function() {
-            $(this).css('color', 'white');
+  $('li').hover(
+    function () {
+      $(this).css('color', hover_colour);
+    },
+    function () {
+      $(this).css('color', 'white');
     });
+});
 
-//});
+
+
+
