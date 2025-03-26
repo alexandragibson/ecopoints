7cfe89b1b0145d357ea51256fb3c985f42886bdd
Geraldine <giquigeraldine@outlook.com>
2025-03-26 19:36:37 +0000

    update django to allow use with onrender update wsgi.py in ecopoints folder update static root and requirements.txt update django in requirements.txt update to staticfiles update allowed hosts add render.yaml for build command add whitenoise for static serving update static tag

--- itech_project/settings.py
+++ itech_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '66=k5u93_7inha(^8bh*u30w@3l)%d(5hdd5ty_7usd%y)2+_('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ecopoints-zewf.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["ecopoints-8mh1.onrender.com", "localhost", "127.0.0.1"]
 
 # Allow access to the Django admin site from phone during development
 # ALLOWED_HOSTS = [
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'itech_project.urls'
@@ -134,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'  # the URL to access the static files
 STATIC_ROOT = STATICFILES_DIR  # where the compressed files will be stored for production
 
-STATICFILES_DIRS = [STATIC_DIR, ]  # where the static files are stored for development
+STATICFILES_DIRS = [STATIC_DIR]  # where the static files are stored for development
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
