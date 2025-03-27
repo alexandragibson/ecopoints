bc3218de7785416f417d622fb4bccbccafe6166d
Gi Ho <giquigeraldine@outlook.com>
2025-03-27 15:41:55 +0000

    update csrf origin

--- ecopoints/settings.py
+++ ecopoints/settings.py
@@ -26,6 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["ecopoints-zewf.onrender.com", "localhost", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "ecopoints-zewf.onrender.com",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
