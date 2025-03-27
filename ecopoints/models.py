a2d5ccc8d74571eb1747264d6fa043a95c720d90
Kimberley Bates <2358219b@student.gla.ac.uk>
2025-03-27 07:09:56 +0000

    models amendment to allow population of completed tasks over multiple days

--- ecopoints/models.py
+++ ecopoints/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+from django.utils import timezone
 
 
 # Create your models here.
@@ -41,7 +42,7 @@ class CompletedTask(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    completed_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         app_label = 'ecopoints'
